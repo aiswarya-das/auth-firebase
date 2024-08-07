62eecf27fc2e3f8a029dee8c0b106967cba2a810
aiswarya-das <aiswaryadas.a@gmail.com>
2024-08-08 04:14:39 +0530

    updated api keys

--- src/libs/firebase/config.ts
+++ src/libs/firebase/config.ts
@@ -1,15 +1,14 @@
 import { getAuth } from 'firebase/auth';
 import { initializeApp, getApps } from 'firebase/app';
 
-// Load .env variables
 const firebaseConfig = {
-  apiKey: "AIzaSyDXQIravq4O9HUluIOV_wOl3eaXulFxKAQ",
-  authDomain: "fire-auth-51629.firebaseapp.com",
-  projectId: "fire-auth-51629",
-  storageBucket: "fire-auth-51629.appspot.com",
-  messagingSenderId: "998529852073",
-  appId: "1:998529852073:web:d5b573ca124b740131ffb8",
-  measurementId: "G-2YMQET2PDF"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const firebaseApp =
